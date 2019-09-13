7032ce8bd4ae1d6ec3a87f928cd9be15d5d72f6d
Eric Seifert <seiferteric@gmail.com>
2019-09-12 20:26:50 -0700

    Set Min TLS Version and cipher suites

--- src/telemetry/telemetry.go
+++ src/telemetry/telemetry.go
@@ -58,6 +58,16 @@ func main() {
 	tlsCfg := &tls.Config{
 		ClientAuth:   tls.RequireAndVerifyClientCert,
 		Certificates: []tls.Certificate{certificate},
+        MinVersion:               tls.VersionTLS12,
+        CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+        PreferServerCipherSuites: true,
+        CipherSuites: []uint16{
+            tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+            tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+            tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+            tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+        },
+
 	}
 	if *allowNoClientCert {
 		// RequestClientCert will ask client for a certificate but won't
