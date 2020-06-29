ce6cc7ebd8bfec25f64a831023da118220af8138
Eric Seifert <seiferteric@gmail.com>
2020-06-29 13:28:08 -0700

    Make gNOI active only on writable mode

--- gnmi_server/server.go
+++ gnmi_server/server.go
@@ -64,7 +64,9 @@ func NewServer(config *Config, opts []grpc.ServerOption) (*Server, error) {
 		return nil, fmt.Errorf("failed to open listener port %d: %v", srv.config.Port, err)
 	}
 	gnmipb.RegisterGNMIServer(srv.s, srv)
-	gnoi_system_pb.RegisterSystemServer(srv.s, srv)
+	if READ_WRITE_MODE {
+		gnoi_system_pb.RegisterSystemServer(srv.s, srv)
+	}
 	log.V(1).Infof("Created Server on %s, read-only: %t", srv.Address(), !READ_WRITE_MODE)
 	return srv, nil
 }
