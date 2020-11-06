01aab03e545b14d0f954a85479b5202085281508
Eric Seifert <seiferteric@gmail.com>
2020-11-06 13:45:52 -0800

    Register Sonic gnoi service. Remove other Sonic gNOI RPCs until a later PR

--- gnmi_server/server.go
+++ gnmi_server/server.go
@@ -17,6 +17,7 @@ import (
 	gnoi_system_pb "github.com/openconfig/gnoi/system"
 	sdc "github.com/Azure/sonic-telemetry/sonic_data_client"
 	gnmipb "github.com/openconfig/gnmi/proto/gnmi"
+	spb_gnoi "github.com/Azure/sonic-telemetry/proto/gnoi"
 	"bytes"
 )
 
@@ -134,6 +135,7 @@ func NewServer(config *Config, opts []grpc.ServerOption) (*Server, error) {
 	}
 	gnmipb.RegisterGNMIServer(srv.s, srv)
 	gnoi_system_pb.RegisterSystemServer(srv.s, srv)
+	spb_gnoi.RegisterSonicServiceServer(srv.s, srv)
 	if READ_WRITE_MODE {
 		gnoi_system_pb.RegisterSystemServer(srv.s, srv)
 	}
