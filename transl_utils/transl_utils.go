be3cb26129f5fa8b04735adbd6d38f7f2d33c2b4
Eric Seifert <seiferteric@gmail.com>
2020-12-01 14:28:46 -0800

    Add yang bundle version check to Bulk set

--- transl_utils/transl_utils.go
+++ transl_utils/transl_utils.go
@@ -260,6 +260,14 @@ func TranslProcessBulk(delete []*gnmipb.Path, replace []*gnmipb.Update, update [
 			Payload: payload,
 			User: translib.UserRoles{Name: rc.Auth.User, Roles: rc.Auth.Roles},
 		}
+		if rc.BundleVersion != nil {
+			nver, err := translib.NewVersion(*rc.BundleVersion)
+			if err != nil {
+				log.V(2).Infof("Action operation failed with error =%v", err.Error())
+				return err
+			}
+			req.ClientVersion = nver
+		}
 		if rc.Auth.AuthEnabled {
 			req.AuthEnabled = true
 		}
@@ -277,6 +285,14 @@ func TranslProcessBulk(delete []*gnmipb.Path, replace []*gnmipb.Update, update [
 			Payload: payload,
 			User: translib.UserRoles{Name: rc.Auth.User, Roles: rc.Auth.Roles},
 		}
+		if rc.BundleVersion != nil {
+			nver, err := translib.NewVersion(*rc.BundleVersion)
+			if err != nil {
+				log.V(2).Infof("Action operation failed with error =%v", err.Error())
+				return err
+			}
+			req.ClientVersion = nver
+		}
 		if rc.Auth.AuthEnabled {
 			req.AuthEnabled = true
 		}
@@ -293,6 +309,14 @@ func TranslProcessBulk(delete []*gnmipb.Path, replace []*gnmipb.Update, update [
 			Path: uri,
 			Payload: payload,
 			User: translib.UserRoles{Name: rc.Auth.User, Roles: rc.Auth.Roles},
+		}
+		if rc.BundleVersion != nil {
+			nver, err := translib.NewVersion(*rc.BundleVersion)
+			if err != nil {
+				log.V(2).Infof("Action operation failed with error =%v", err.Error())
+				return err
+			}
+			req.ClientVersion = nver
 		}
 		if rc.Auth.AuthEnabled {
 			req.AuthEnabled = true
